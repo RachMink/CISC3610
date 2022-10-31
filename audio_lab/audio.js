634550be77a49c28c7325be54d085003966342f1
Rachel-Minkowitz <rhminkowitz@gmail.com>
2022-10-30 22:42:50 -0400

    audio lab changes

---
+++ audio_lab/audio.js
@@ -0,0 +1,35 @@
+const accordion = new Audio("sounds/accordion.mp3");
+const trumpet = new Audio("sounds/trumpet.mp3");
+const drum = new Audio("sounds/drum.mp3");
+const symbol = new Audio("sounds/symbol.mp3");
+const trombone = new Audio("sounds/trombone.mp3");
+const tamborine = new Audio("sounds/tamborine.mp3");
+const triangle = new Audio("sounds/triangle.mp3");
+
+function playAccordion(){
+    accordion.play();
+}
+
+function trumpetPlay(){
+    trumpet.play();
+}
+
+function drumPlay(){
+    drum.play();
+}
+
+function symbolPlay(){
+    symbol.play();
+}
+
+function trombonePlay(){
+    trombone.play();
+}
+
+function tamborinePlay(){
+    tamborine.play();
+}
+
+function trianglePlay(){
+    triangle.play();
+}
